162f5ca587d40ec915780a5a53ac15cdf8ea4c52
bahriddinovfirdavs <firdavsb762@gmail.com>
2023-09-12 12:21:06 -0700

    add solution

--- While05.py
+++ While05.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
+    i=0
+    n=len(s)
+    ans=0
+    while i<n:
+        if s[i] in 'qwertyuiopasdfghjklzxcvbnm':
+            ans+=1
+        i+=1
+    return ans
 
