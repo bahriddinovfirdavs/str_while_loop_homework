c913d2a16da0ccc5acc892399555e03a75f6af0e
bahriddinovfirdavs <firdavsb762@gmail.com>
2023-09-12 12:16:33 -0700

    add solution

--- While03.py
+++ While03.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-
+    i=0
+    n=len(s)
+    ans=0
+    while i<n:
+        if s[i] in '`~!@#$%^&*()_-+=|\}]{[":;?/><,.]}':
+            ans+=1
+        i+=1
+    return ans
